7f6310c064e634ca5e02c7ad197ccac5373ef196
Amee Covarrubias <ameecova@yahoo.com>
2018-07-19 12:10:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase? == false 
     return "HUH?! SPEAK UP, SONNY!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == ""
+    return "NO, NOT SINCE 1938!"
   end
 end
