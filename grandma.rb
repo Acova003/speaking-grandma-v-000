c28d76d75158b5efc3318f3819eb0c25889011ca
Amee Covarrubias <ameecova@yahoo.com>
2018-07-19 12:23:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 require 'pry'
 def speak_to_grandma(phrase)
-  bindind.pry
+  binding.pry
   if phrase.upcase? == false 
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
